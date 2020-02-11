fa7c07b902ce1369f726fa901fb4481124b4587f
gur50172 <shashank.kaushik@altran.com>
2020-02-11 17:20:18 +0530

    Side bars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    BottomBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
